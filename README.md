ac663564c0bf4d8772e95292e0ff8e95ba394574
MightyCoderX <mattiaca003@gmail.com>
2020-06-02 23:01:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JavaEditor
+ A very basic java editor which can compile and run java programs
---
+++ src/com/mightycoderx/javafx/Controller.java
@@ -0,0 +1,167 @@
+package com.mightycoderx.javafx;
+
+import com.sun.istack.internal.Nullable;
+import javafx.concurrent.Worker;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+    @FXML
+    private WebView editor;
+
+    @FXML
+    public HBox buttonBox;
+
+    @FXML
+    public Button btnCompile;
+
+    @FXML
+    private Button btnRun;
+
+    @FXML
+    public Button btnCompileAndRun;
+
+    @FXML
+    private TextArea txtConsole;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        File file = new File("Main.java");
+
+        editor.getEngine().setJavaScriptEnabled(true);
+        editor.getEngine().load(Controller.class.getResource("editor.html").toExternalForm().replace("file:/", "file:///"));
+
+        editor.getEngine().getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == Worker.State.SUCCEEDED)
+            {
+                editor.getEngine().executeScript("initEditor()");
+                buttonBox.setDisable(false);
+            }
+        });
+
+        final KeyCombination copyCombo = new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN);
+
+        editor.addEventFilter(KeyEvent.KEY_PRESSED, e ->
+        {
+            if (copyCombo.match(e)) {
+                onCopy();
+                onCopy();
+            }
+        });
+
+        btnCompile.setOnAction(e ->
+        {
+            compile(file);
+            btnRun.setDisable(false);
+        });
+
+        btnRun.setOnAction(e -> run(file));
+
+        btnCompileAndRun.setOnAction(e ->
+        {
+            compile(file);
+            run(file);
+        });
+    }
+
+    private void onCopy()
+    {
+        String contentText = (String) editor.getEngine().executeScript("copySelection()");
+
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        content.putString(contentText);
+        clipboard.setContent(content);
+    
+        System.out.println("Clipboard: " + clipboard.getString());
+    }
+
+    public void compile(File file)
+    {
+        try
+        {
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file.getPath());
+            writer.write((String) editor.getEngine().executeScript("getValue()"));
+            writer.close();
+
+            Process pr = Runtime.getRuntime().exec("javac " + file.getPath());
+            int exitCode = printProcessOutput(pr);
+            if(exitCode == 0)
+            {
+                println("Compiled successfully!\n");
+            }
+        }
+        catch (IOException | InterruptedException ioException)
+        {
+            ioException.printStackTrace();
+        }
+    }
+
+    public void run(File file)
+    {
+        if(!txtConsole.getText().isEmpty())
+        {
+            txtConsole.clear();
+        }
+
+        try
+        {
+            Process pr =  Runtime.getRuntime().exec("java " + file.getPath().replace(".java", ""));
+            int exitCode = printProcessOutput(pr);
+            println("Process ended with exit code " + exitCode);
+        }
+        catch (IOException | InterruptedException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    public int printProcessOutput(Process pr) throws InterruptedException
+    {
+        try
+        {
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+
+            String s = null;
+            while ((s = stdInput.readLine()) != null)
+            {
+                println(s);
+            }
+
+            s = null;
+            while ((s = stdError.readLine()) != null)
+            {
+                println(s);
+            }
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return pr.waitFor();
+    }
+
+    public void println(@Nullable String str)
+    {
+        if(str == null)
+        {
+            str = "";
+        }
+        txtConsole.setText(txtConsole.getText() + str + "\n");
+    }
+}
